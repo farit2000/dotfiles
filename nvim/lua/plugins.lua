d2480d57ae98755fe6780f3cb4e8238330ec0a36
Farit Shamardanov <42946448+farit2000@users.noreply.github.com>
2023-10-28 14:38:27 +0300

    Update plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -6,6 +6,39 @@ require('packer').startup(function(use)
 	use 'nvim-treesitter/nvim-treesitter-context'
 	use {'crispgm/nvim-tabline', config = function() require('tabline').setup({}) end, }
 	use 'neovim/nvim-lspconfig'
+	use 'lewis6991/gitsigns.nvim'
+	use 'mfussenegger/nvim-dap'
+	use 'leoluz/nvim-dap-go'
+
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/nvim-cmp'
+	use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+	use 'L3MON4D3/LuaSnip' -- Snippets plugin
+	use 'fatih/vim-go'
+	use {
+		'nvim-tree/nvim-tree.lua',
+		requires = {
+			'nvim-tree/nvim-web-devicons', -- optional
+		},
+	}
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.2',
+-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use {
+		'numToStr/Comment.nvim',
+		config = function()
+			require('Comment').setup()
+		end
+	}
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function() vim.fn["mkdp#util#install"]() end,
+	})
 end)
 
 require('onedark').load()
@@ -15,8 +48,53 @@ require('onedark').setup  {
 	style = 'cool', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
 }
 
+require('gitsigns').setup{
+	on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
+
+		local function map(mode, l, r, opts)
+    		  opts = opts or {}
+    		  opts.buffer = bufnr
+    		  vim.keymap.set(mode, l, r, opts)
+    		end
+
+    		-- Navigation
+    		map('n', ']c', function()
+    		  if vim.wo.diff then return ']c' end
+    		  vim.schedule(function() gs.next_hunk() end)
+    		  return '<Ignore>'
+    		end, {expr=true})
+
+    		map('n', '[c', function()
+    		  if vim.wo.diff then return '[c' end
+    		  vim.schedule(function() gs.prev_hunk() end)
+    		  return '<Ignore>'
+    		end, {expr=true})
+
+		vim.keymap.set('n', '<leader>hs', gs.stage_hunk)
+    		vim.keymap.set('n', '<leader>hr', gs.reset_hunk)
+    		vim.keymap.set('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    		vim.keymap.set('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    		vim.keymap.set('n', '<leader>hS', gs.stage_buffer)
+    		vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk)
+    		vim.keymap.set('n', '<leader>hR', gs.reset_buffer)
+    		vim.keymap.set('n', '<leader>hp', gs.preview_hunk)
+    		vim.keymap.set('n', '<leader>hb', function() gs.blame_line{full=true} end)
+    		vim.keymap.set('n', '<leader>tb', gs.toggle_current_line_blame)
+    		vim.keymap.set('n', '<leader>hd', gs.diffthis)
+    		vim.keymap.set('n', '<leader>hD', function() gs.diffthis('~') end)
+    		vim.keymap.set('n', '<leader>td', gs.toggle_deleted)
+
+		map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+	end
+}
+
+require("nvim-tree").setup()
+
+require('Comment').setup()
+
 require 'nvim-treesitter.configs'.setup {
-	ensure_installed = { "c", "lua", "vim", "help" },
+	ensure_installed = { "c", "lua", "vim", "vimdoc" },
 	sync_install = false,
 	auto_install = true,
 
@@ -25,7 +103,64 @@ require 'nvim-treesitter.configs'.setup {
 	},
 }
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require('telescope').setup{
+	defaults = {
+		vimgrep_arguments = {
+			'rg',
+			'--color=never',
+			'--no-heading',
+			'--with-filename',
+			'--line-number',
+			'--column',
+			'--smart-case',
+			'-u' -- thats the new thing
+		},
+	},
+	pickers = {
+		find_files = {
+			no_ignore = true,
+		}
+	}
+}
+
 require 'treesitter-context'.setup{}
+
+require('dap-go').setup()
+
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
+vim.keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+vim.keymap.set('n', '<Leader>bal', function() require('dap').list_breakpoints() end)
+vim.keymap.set('n', '<Leader>bac', function() require('dap').clear_breakpoints() end)
+vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
+	require('dap.ui.widgets').hover()
+end)
+vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
+	require('dap.ui.widgets').preview()
+end)
+vim.keymap.set('n', '<Leader>df', function()
+	local widgets = require('dap.ui.widgets')
+	widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set('n', '<Leader>ds', function()
+	local widgets = require('dap.ui.widgets')
+	widgets.centered_float(widgets.scopes)
+end)
+
+vim.keymap.set('n', '<Leader>dt', function() require('dap-go').debug_test() end)
+vim.keymap.set('n', '<Leader>dlt', function() require('dap-go').debug_last_test() end)
 
 local lspconfig = require('lspconfig')
 local util = require('lspconfig/util')
@@ -37,12 +172,16 @@ lspconfig.perlpls.setup{}
 
 lspconfig.gopls.setup{
 	cmd = { 'gopls' },
-	capabilities = capabilities,
 	settings = {
 		gopls = {
 			analyses = {
 				unusedparams = true,
 				shadow = true,
+				unusedwrite = true,
+				fieldalignment = true,
+				nilness = true,
+				useany = true,
+				unusedvariable = true,
 			},
 			staticcheck = true,
 		},
@@ -62,7 +201,7 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
 	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	 --vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 	
 	-- Mappings.
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
@@ -82,7 +221,85 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
 	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+	vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+	vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+	vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
 end
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+	},
+  mapping = cmp.mapping.preset.insert({
+      ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+      ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+
+      ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+      ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+
+      ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-d>'] = cmp.mapping.scroll_docs(4),
+
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+      ['<C-f>'] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(1) then
+          luasnip.jump(1)
+        else
+          fallback()
+        end
+      end, {'i', 's'}),
+
+     ['<C-b>'] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {'i', 's'}),
+
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      local col = vim.fn.col('.') - 1
+
+      if cmp.visible() then
+        cmp.select_next_item(select_opts)
+      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+        fallback()
+      else
+        cmp.complete()
+      end
+    end, {'i', 's'}),
+
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item(select_opts)
+      else
+        fallback()
+      end
+    end, {'i', 's'}),
+    }),
+  sources = cmp.config.sources({
+	{name = 'path'},
+	{ name = 'nvim_lsp' },
+	{ name = 'luasnip' },
+})
+}
+
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
@@ -90,9 +307,13 @@ local servers = { 'clangd' , 'perlpls', 'gopls' }
 for _, lsp in pairs(servers) do
 	require('lspconfig')[lsp].setup {
 		on_attach = on_attach,
-			flags = {
-				-- This will be the default in neovim 0.7+
-				debounce_text_changes = 150,
-			}
+		capabilities = capabilities,
+		flags = {
+		-- This will be the default in neovim 0.7+
+			debounce_text_changes = 150,
 		}
+	}
 end
+
+-- global
+vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
